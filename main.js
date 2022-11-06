d7869d784fd837ae7c04b47ad2cca09058431447
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-06 14:05:21 -0500

    commit tree 8e111f5b8cc2ff784a73b2f13c6b5109f9464a95

--- main.js
+++ main.js
@@ -4,8 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeheart = document.querySelectorAll('.like-glyph');
 
+likeheart.forEach((glyph) => {
+  glyph.addEventListener('click', (e) => {
+    const heart = e.target
+    console.log(heart)
+    mimicServerCall()
+    .then(() => {
+      if (heart.textContent === EMPTY_HEART){
+        heart.textContent = FULL_HEART
+        heart.className = 'activated-heart'
+      }else if(heart.textContent === FULL_HEART){
+        heart.textContent = EMPTY_HEART
+        heart.className = ""
+      }
+    })
+    .catch((error) => {
+      const modal = document.querySelector('#modal-message')
+      modal.className = ""
+      modal.textContent = error
+      setTimeout(() => modal.className = 'hidden', 2000)
+    })
 
+  })
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
